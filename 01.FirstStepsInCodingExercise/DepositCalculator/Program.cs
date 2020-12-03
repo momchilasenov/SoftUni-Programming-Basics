f92e55f18452974b68edc046a3f1675b2cfc47be
momchilasenov <momchil276@gmail.com>
2020-12-03 09:26:45 +0200

    Added First Steps in Coding Exercise assignments

---
+++ 01.FirstStepsInCodingExercise/DepositCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inputs
+            double deposit = double.Parse(Console.ReadLine());
+            int period = int.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine());
+            //Calculations
+            double totalInterest = deposit * interest / 100;
+            double monthlyInterest = totalInterest / 12;
+            double sum = deposit + period * monthlyInterest;
+            //Output
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
