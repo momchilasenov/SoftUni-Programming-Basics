c7db856543042558b4ab3e6f6cb42165f8e786c4
momchilasenov <momchil276@gmail.com>
2020-12-04 15:50:24 +0200

    Added Simple Calculations lab assignments

---
+++ 02.SimpleCalculations/PetShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int others = int.Parse(Console.ReadLine());
+            double result = (dogs * 2.50) + (others * 4);
+            Console.WriteLine($"{result} lv.");
+
+        }
+    }
+}
---
+++ 02.SimpleCalculations/YardGreening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double totalprice = meters * 7.61;
+            double discount = totalprice * 0.18;
+            double finalprice = totalprice - discount;
+
+            Console.WriteLine($"The final price is: {finalprice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
