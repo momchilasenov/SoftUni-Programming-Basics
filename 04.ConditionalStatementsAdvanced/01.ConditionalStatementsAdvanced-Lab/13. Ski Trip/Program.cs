8f7e0934dc76ad4c693d6184e98e2bcda2c5c6db
momchilasenov <momchil276@gmail.com>
2020-12-06 17:10:14 +0200

    Added Conditional Statements Advanced assignments

---
+++ 04.ConditionalStatementsAdvanced/01.ConditionalStatementsAdvanced-Lab/13. Ski Trip/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace _13._Ski_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. входни данни
+            int days = int.Parse(Console.ReadLine());
+            string room = Console.ReadLine();
+            string review = Console.ReadLine();
+            
+            double price = 0;
+            double discount = 0;
+            // 2. определяме price и discount                
+            if (room == "room for one person")
+            {
+                price = 18.00;
+            }
+            else if (room == "apartment")
+            {
+                price = 25.00;
+
+                if (days < 10)
+                {
+                    discount = 0.3;
+                }
+                else if (days >= 10 && days <= 15)
+                {
+                    discount = 0.35;
+                }
+                else if (days > 15)
+                {
+                    discount = 0.5;
+                }
+            }
+            else if (room == "president apartment")
+            {
+                price = 35.00;
+                
+                if (days < 10)
+                {
+                    discount = 0.1;
+                }
+                else if (days >= 10 && days <= 15)
+                {
+                    discount = 0.15;
+                }
+                else if (days > 15)
+                {
+                    discount = 0.2;
+                }
+            }
+
+            // 3. изчисляване на цената 
+            double total = price * (days - 1);
+            // 4. приспадане на намаление от цената
+            total -= total * discount;
+
+            // 5. позитивна или негативна оценка
+            if (review == "positive")
+            {
+                total += total * 0.25;
+            }
+            else if (review == "negative")
+            {
+                total -= total * 0.1;
+            }
+            // 6. принтираме
+
+            Console.WriteLine($"{total:f2}");
+
+
+        }
+    }
+}
