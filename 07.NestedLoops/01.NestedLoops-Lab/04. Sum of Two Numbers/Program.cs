327a45080591af2e3518c138ca83e194ce4a8b74
momchilasenov <momchil276@gmail.com>
2020-12-09 09:56:15 +0200

    Added Nested Loops Lab assignment

---
+++ 07.NestedLoops/01.NestedLoops-Lab/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startInterval = int.Parse(Console.ReadLine());
+            int endInterval = int.Parse(Console.ReadLine()); 
+            int magicNumber = int.Parse(Console.ReadLine()); 
+            int combinationCounter = 0;
+
+            for (int i = startInterval; i <= endInterval; i++)
+            {
+                for (int j = startInterval; j <= endInterval; j++)
+                {
+                    combinationCounter++;
+                    int sum = i + j;
+                    if (sum == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinationCounter} ({i} + {j} = {magicNumber})");
+                        return;
+                    }
+
+                }
+            }
+            Console.WriteLine($"{combinationCounter} combinations - neither equals {magicNumber}");
+
+        }
+    }
+}
