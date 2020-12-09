e0ced5c5507ba97bc25345e2eb177be06a207650
momchilasenov <momchil276@gmail.com>
2020-12-09 09:57:54 +0200

    Added Nested Loops Lab assignment

---
+++ 07.NestedLoops/01.NestedLoops-Lab/05. Travelling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string destination = Console.ReadLine();
+
+            while (destination != "End") 
+            {
+                double budget = double.Parse(Console.ReadLine()); 
+                double moneySaved = 0;
+                double totalMoneySaved = 0;
+
+                while (totalMoneySaved < budget) 
+                {
+                    moneySaved = double.Parse(Console.ReadLine());
+                    totalMoneySaved += moneySaved;
+                }
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+
+        }
+    }
+}
