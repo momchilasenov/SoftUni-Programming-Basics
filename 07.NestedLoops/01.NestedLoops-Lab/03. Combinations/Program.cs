613b2b6a6da10ac463481aa6cb50c366a0d15463
momchilasenov <momchil276@gmail.com>
2020-12-09 09:55:19 +0200

    Added Nested Loops Lab assignment

---
+++ 07.NestedLoops/01.NestedLoops-Lab/03. Combinations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countCombinations = 0;
+
+            for (int i = 0; i<=n; i++)
+            {
+                for (int j = 0; j<=n; j++)
+                {
+                    for (int k = 0; k<=n; k++)
+                    {
+                        int value = i + j + k;
+                        if (value == n)
+                        {
+                            countCombinations++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(countCombinations);
+
+        }
+    }
+}
