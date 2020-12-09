d6bb4da4bc47f815f0ab2a95c1eb5f55ce527b6c
momchilasenov <momchil276@gmail.com>
2020-12-09 10:03:27 +0200

    Added Nested Loops Lab assignment

---
+++ 07.NestedLoops/01.NestedLoops-Lab/08. Password Hacking/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Password_Hacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            for (char i = 'a'; i <= 'z'; i++) 
+            {
+                for (char j = 'a'; j <= 'z'; j++)
+                {
+                    for (char k = 'a'; k <= 'z'; k++)
+                    {
+                        for (char l = 'a'; l <= 'z'; l++)
+                        {
+                            string combination = $"{i}{j}{k}{l}";
+                            if (combination == password)
+                            {
+                                Console.WriteLine($"You cracked it. Password is {combination}");
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
