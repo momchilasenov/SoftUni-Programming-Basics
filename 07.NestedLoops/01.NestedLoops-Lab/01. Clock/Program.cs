73936f4269dd45c0794153329a3af4ea0faa93c5
momchilasenov <momchil276@gmail.com>
2020-12-09 09:51:54 +0200

    Added Nested Loops assignment

---
+++ 07.NestedLoops/01.NestedLoops-Lab/01. Clock/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int hour = 0; hour <= 23; hour++)  
+            {
+                for (int minutes = 0; minutes <= 59; minutes++)
+                {
+                    for (int seconds = 0; seconds <= 59; seconds++)
+                    {
+                        Console.WriteLine($"{hour}:{minutes}:{seconds}");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
