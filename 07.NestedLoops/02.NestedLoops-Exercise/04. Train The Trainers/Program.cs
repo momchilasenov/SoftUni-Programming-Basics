7757500174da57f5e330e482d502acdc9d5a7662
momchilasenov <momchil276@gmail.com>
2020-12-10 09:38:47 +0200

    Added Nested loop exercise assignment

---
+++ 07.NestedLoops/02.NestedLoops-Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jury = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            double grade = 0; 
+            double totalGrades = 0;
+            
+            while (command != "Finish")
+            { 
+                double averageGrade = 0; 
+                double currentPresentationGradesSum = 0; 
+                for (int i = 1; i <= jury; i++) 
+                {
+                    grade = double.Parse(Console.ReadLine());
+                    currentPresentationGradesSum += grade;
+                    totalGrades += grade;
+                    gradeCounter++;
+                }
+                averageGrade = currentPresentationGradesSum / jury;
+                Console.WriteLine($"{command} - {averageGrade:f2}.");
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Student's final assessment is {totalGrades / gradeCounter:f2}.");
+
+        }
+    }
+}
