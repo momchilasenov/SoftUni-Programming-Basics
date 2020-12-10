e96b55f51dc1187acaf6c441c119d9aa568c40a1
momchilasenov <momchil276@gmail.com>
2020-12-10 09:30:06 +0200

    Added Nested loops exercise assignment

---
+++ 07.NestedLoops/02.NestedLoops-Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNumber = 1;
+            bool flag = false;
+
+            for (int i = 1; i <= n; i++) 
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (currentNumber > n)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    Console.Write(currentNumber + " ");
+                    currentNumber++;
+                }
+
+                if (flag)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
