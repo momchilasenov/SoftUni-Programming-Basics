b615f8354bb529c20c7ffc199de09181ad5eba26
momchilasenov <momchil276@gmail.com>
2020-12-11 18:23:27 +0200

    Added Nested Loops more exercises assignments

---
+++ 07.NestedLoops/03.NestedLoops-MoreExercises/12. The song of the wheels/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _12._The_song_of_the_wheels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int controlNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool flag = false;
+            int passA = 0;
+            int passB = 0;
+            int passC = 0;
+            int passD = 0;
+            int newLine = 0;
+
+
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 1; b <= 9; b++)
+                {
+                    for (int c = 1; c <= 9; c++)
+                    {
+                        for (int d = 1; d <= 9; d++)
+                        {
+                            if (a * b + c * d == controlNumber)
+                            {
+                                if (a < b && c > d)
+                                {
+                                    counter++;
+                                    Console.Write($"{a}{b}{c}{d} ");
+                                    newLine = 1;
+                                    if (counter == 4)
+                                    {
+                                        passA = a;
+                                        passB = b;
+                                        passC = c;
+                                        passD = d;
+                                        flag = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine($"{Environment.NewLine}Password: {passA}{passB}{passC}{passD}");
+            }
+            else if (!flag)
+            {
+                if (newLine == 1)
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine("No!");
+            }
+
+        }
+    }
+}
---
+++ 07.NestedLoops/03.NestedLoops-MoreExercises/13. Prime Pairs/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _13._Prime_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startFirstPair = int.Parse(Console.ReadLine());
+            int startSecondPair = int.Parse(Console.ReadLine());
+            int diffFirstPair = int.Parse(Console.ReadLine());
+            int diffSecondPair = int.Parse(Console.ReadLine());
+
+            int endFirstPair = startFirstPair + diffFirstPair;
+            int endSecondPair = startSecondPair + diffSecondPair;
+
+            int count1 = 0;
+            int count2 = 0;
+
+            for (int i = startFirstPair; i <= endFirstPair; i++)
+            {
+                for (int d1 = 1; d1 <= i; d1++)
+                {
+                    if (i % d1 == 0)
+                    {
+                        count1++;
+                        if (count1 > 2)
+                        {
+                            count1 = 0;
+                            break;
+                        }
+                    }
+                }
+
+                for (int j = startSecondPair; j <= endSecondPair; j++)
+                {
+                    for (int d2 = 1; d2 <= j; d2++)
+                    {
+                        if (j % d2 == 0)
+                        {
+                            count2++;
+                            if (count2 > 2)
+                            {
+                                count2 = 0;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (count1 == 2 && count2 == 2)
+                    {
+                        Console.WriteLine($"{i}{j}");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
