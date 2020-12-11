5e323c6b26e0f2d45755ae1cedf9ea1a202d99d6
momchilasenov <momchil276@gmail.com>
2020-12-11 18:22:03 +0200

    Added Nested loops exercise assignments

---
+++ 07.NestedLoops/03.NestedLoops-MoreExercises/11. HappyCat Parking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11._HappyCat_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDays = int.Parse(Console.ReadLine());
+            int hoursPerDay = int.Parse(Console.ReadLine());
+            double price = 0;
+            double total = 0;
+
+            for (int day=1; day<=numberOfDays; day++)
+            {
+                double sum = 0;
+                for (int hour=1; hour<=hoursPerDay; hour++)
+                {
+                    if (day % 2 == 0 && hour%2!=0)
+                    {
+                        price = 2.50;
+                    }
+                    else if (day % 2 != 0 && hour % 2 == 0)
+                    {
+                        price = 1.25;
+                    }
+                    else
+                    {
+                        price = 1;
+                    }
+                    sum += price;
+                                      
+                }
+                total += sum;
+                Console.WriteLine($"Day: {day} - {sum:f2} leva");
+            }
+            Console.WriteLine($"Total: {total:f2} leva");
+        }
+    }
+}
