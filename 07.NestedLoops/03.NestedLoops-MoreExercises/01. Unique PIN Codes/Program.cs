847927d48ab7bb6dc73f81dba6f3370c9efa6b05
momchilasenov <momchil276@gmail.com>
2020-12-11 18:17:04 +0200

    Added Nested loops more exercise assignments

---
+++ 07.NestedLoops/03.NestedLoops-MoreExercises/01. Unique PIN Codes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Unique_PIN_Codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumberLimit = int.Parse(Console.ReadLine());
+            int secondNumberLimit = int.Parse(Console.ReadLine());
+            int thirdNumberLimit = int.Parse(Console.ReadLine());
+
+            for (int firstNumber = 1; firstNumber <= firstNumberLimit; firstNumber++)
+            {
+
+                for (int secondNumber = 1; secondNumber <= secondNumberLimit; secondNumber++)
+                {
+                    int counter = 0; //зануляваме брояча тук за да е на 0 за всяко ново число
+                    for (int i = 1; i <= secondNumber; i++)
+                    {
+                        if (secondNumber % i == 0)
+                        {
+                            counter++;
+                        }
+                    }
+                    for (int thirdNumber = 1; thirdNumber <= thirdNumberLimit; thirdNumber++)
+                    {
+                        if (firstNumber % 2 == 0 && thirdNumber % 2 == 0)
+                        {
+                            if (counter == 2)
+                            {
+                                Console.WriteLine($"{firstNumber} {secondNumber} {thirdNumber}");
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
---
+++ 07.NestedLoops/03.NestedLoops-MoreExercises/02. Letters Combinations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02._Letters_Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char startLetter = char.Parse(Console.ReadLine());
+            char endLetter = char.Parse(Console.ReadLine());
+            char tabooLetter = char.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (char i = startLetter; i <= endLetter; i++)
+            {
+                for (char j = startLetter; j <= endLetter; j++)
+                {
+                    for (char k = startLetter; k <= endLetter; k++)
+                    {
+                        if (i == tabooLetter || j == tabooLetter || k == tabooLetter)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.Write($"{i}{j}{k} ");
+                            count++;
+
+                        }
+                    }
+                }
+            }
+            Console.Write($"{count}");
+        }
+    }
+}
+
+
+
