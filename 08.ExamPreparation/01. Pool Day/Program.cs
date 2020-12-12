f04053068616b3f43f925a546e32eed988aa4e6b
momchilasenov <momchil276@gmail.com>
2020-12-12 20:59:55 +0200

    Added exam problems

---
+++ 08.ExamPreparation/01. Pool Day/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Pool_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            double entranceFee = double.Parse(Console.ReadLine());
+            double chairPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+
+            double totalEntrance = numberOfPeople * entranceFee;
+            double chairsNeeded = Math.Ceiling(numberOfPeople * 0.75);
+            double umbrellasNeeded = Math.Ceiling(numberOfPeople * 0.5);
+
+            double priceOfChairs = chairsNeeded * chairPrice;
+            double priceOfUmbrellas = umbrellasNeeded * umbrellaPrice;
+
+            double total = priceOfChairs + priceOfUmbrellas + totalEntrance;
+
+            Console.WriteLine($"{total:f2} lv.");
+        }
+    }
+}
---
+++ 08.ExamPreparation/01. Repainting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylonNeeded = int.Parse(Console.ReadLine());
+            int paintNeeded = int.Parse(Console.ReadLine());
+            int letersOfMixer = int.Parse(Console.ReadLine());
+            int hoursForWorkers = int.Parse(Console.ReadLine());
+
+            double nylonPrice = (nylonNeeded + 2) * 1.50;
+            double paintPrice = (paintNeeded + paintNeeded * 0.1) * 14.50;
+            double mixerPrice = letersOfMixer * 5;
+
+            double totalPrice = nylonPrice + paintPrice + mixerPrice + 0.40;
+
+            double workersSalaryPerHour = totalPrice * 0.3;
+            double workersSalary = workersSalaryPerHour * hoursForWorkers;
+
+            double endTotal = totalPrice + workersSalary;
+
+            Console.WriteLine($"Total expenses: {endTotal:f2} lv.");
+        }
+    }
+}
