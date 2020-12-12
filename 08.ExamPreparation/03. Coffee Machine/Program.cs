67a14ed2bc8c8ca039251a684984647b2238d560
momchilasenov <momchil276@gmail.com>
2020-12-12 21:15:39 +0200

    Added exam problem

---
+++ 08.ExamPreparation/03. Coffee Machine/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _03._Coffee_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string sugar = Console.ReadLine();
+            int numberOfDrinks = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (drink == "Espresso")
+            {
+                if (sugar == "Without")
+                {
+                    price = 0.90;
+                }
+                else if (sugar == "Normal")
+                {
+                    price = 1;
+                }
+                else if (sugar == "Extra")
+                {
+                    price = 1.20;
+                }
+            }
+            else if (drink == "Cappuccino")
+            {
+                if (sugar == "Without")
+                {
+                    price = 1;
+                }
+                else if (sugar == "Normal")
+                {
+                    price = 1.20;
+                }
+                else if (sugar == "Extra")
+                {
+                    price = 1.60;
+                }
+            }
+            else if (drink == "Tea")
+            {
+                if (sugar == "Without")
+                {
+                    price = 0.5;
+                }
+                else if (sugar == "Normal")
+                {
+                    price = 0.6;
+                }
+                else if (sugar == "Extra")
+                {
+                    price = 0.7;
+                }
+            }
+
+            double total = numberOfDrinks * price;
+
+            if (sugar == "Without")
+            {
+                total -= total * 0.35; 
+            }
+            if (drink == "Espresso" && numberOfDrinks >= 5)
+            {
+                total -= total * 0.25;
+            }
+            if (total > 15)
+            {
+                total -= total * 0.2;
+            }
+
+            Console.WriteLine($"You bought {numberOfDrinks} cups of {drink} for {total:f2} lv.");
+        }
+    }
+}
