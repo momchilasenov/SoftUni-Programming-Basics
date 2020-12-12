5ae7383b45fadff45935c9d6efec85ae402e2710
momchilasenov <momchil276@gmail.com>
2020-12-12 22:16:41 +0200

    Added exam problems

---
+++ 08.ExamPreparation/04. Club/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04._Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double desiredProfit = double.Parse(Console.ReadLine());
+            double totalProfit = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "Party!")
+            {
+                int numberOfCoctails = int.Parse(Console.ReadLine());
+                int priceOfCoctail = command.Length;
+                double totalForCoctail = numberOfCoctails * priceOfCoctail;
+
+                if (totalForCoctail % 2 != 0)
+                {
+                    totalForCoctail -= totalForCoctail * 0.25;
+                }
+                totalProfit += totalForCoctail;
+
+                if (totalProfit >= desiredProfit)
+                {
+                    Console.WriteLine("Target acquired.");
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+            if (command == "Party!")
+            {
+                Console.WriteLine($"We need {desiredProfit - totalProfit:f2} leva more.");
+
+            }
+            Console.WriteLine($"Club income - {totalProfit:f2} leva.");
+        }
+    }
+}
---
+++ 08.ExamPreparation/04. Renovation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04._Renovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wallHeight = int.Parse(Console.ReadLine());
+            int wallWidth = int.Parse(Console.ReadLine());
+            int percentageToNotPaint = int.Parse(Console.ReadLine());
+
+            int area = wallHeight * wallWidth * 4;
+            double areaToPaint = Math.Ceiling(area - (area * ((percentageToNotPaint * 1.0) / 100)));
+
+            string command = Console.ReadLine();
+
+            while (command != "Tired!")
+            {
+                int letersOfPaint = int.Parse(command);
+
+                areaToPaint -= letersOfPaint;
+
+                if (areaToPaint==0)
+                {
+                    Console.WriteLine($"All walls are painted! Great job, Pesho!");
+                    break;
+                }
+                if (areaToPaint <= 0 && letersOfPaint > 0)
+                {
+                    Console.WriteLine($"All walls are painted and you have {Math.Abs(areaToPaint)} l paint left!");
+                    break;
+                }
+
+
+                command = Console.ReadLine();
+            }
+            if (command == "Tired!")
+            {
+                Console.WriteLine($"{areaToPaint} quadratic m left.");
+            }
+            
+
+        }
+    }
+}
