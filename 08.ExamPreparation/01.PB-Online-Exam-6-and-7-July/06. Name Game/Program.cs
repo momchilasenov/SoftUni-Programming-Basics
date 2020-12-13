257c5adcadd9b1abdb5b2e22c31ee479a020b425
momchilasenov <momchil276@gmail.com>
2020-12-13 17:08:11 +0200

    Added exam problems

---
+++ 08.ExamPreparation/01.PB-Online-Exam-6-and-7-July/06. Name Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Name_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 08.ExamPreparation/02.PB-Online-Exam-6-and-7-April/01. Oscars ceremony/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Oscars_ceremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int roomRent = int.Parse(Console.ReadLine());
+
+            double statues = roomRent - roomRent * 0.3;
+            double food = statues - statues * 0.15;
+            double music = food * 0.5;
+
+            double total = roomRent + statues + food + music;
+            Console.WriteLine($"{total:f2}");
+
+        }
+    }
+}
