c270ae464e1110370a6a9fb27f5a35cad517c37d
momchilasenov <momchil276@gmail.com>
2020-12-13 17:04:07 +0200

    Added exam problems

---
+++ 08.ExamPreparation/05. Football Tournament/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _05._Football_Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfTeam = Console.ReadLine();
+            int gamesPlayed = int.Parse(Console.ReadLine());
+
+            int points = 0;
+            int totalPoints = 0;
+            int totalGames = 0;
+
+            int wins = 0;
+            int draws = 0;
+            int losses = 0;
+
+            if (gamesPlayed == 0)
+            {
+                Console.WriteLine($"{nameOfTeam} hasn't played any games during this season.");
+                return;
+            }
+
+            for (int i = 1; i<= gamesPlayed; i++)
+            {
+                char result = char.Parse(Console.ReadLine());
+
+                if (result == 'W')
+                {
+                    points = 3;
+                    wins++;
+                }
+                else if (result == 'D')
+                {
+                    points = 1;
+                    draws++;
+                }
+                else if (result == 'L')
+                {
+                    points = 0;
+                    losses++;
+                }
+                totalPoints += points;
+                totalGames = wins + draws + losses;
+
+            }
+            Console.WriteLine($"{nameOfTeam} has won {totalPoints} points during this season.");
+            Console.WriteLine("Total stats:");
+            Console.WriteLine($"## W: {wins}");
+            Console.WriteLine($"## D: {draws}");
+            Console.WriteLine($"## L: {losses}");
+            Console.WriteLine($"Win rate: {wins*1.0/totalGames*100:f2}%");
+   
+        }
+    }
+}
---
+++ 08.ExamPreparation/05. PC Game Shop/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05._PC_Game_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soldGames = int.Parse(Console.ReadLine());
+
+            int hearthstone = 0;
+            int fornite = 0;
+            int overwatch = 0;
+            int others = 0;
+            double total = 0;
+
+            for (int i = 1; i<=soldGames; i++)
+            {
+                string gameName = Console.ReadLine();
+
+                if (gameName == "Hearthstone")
+                {
+                    hearthstone++;
+                }
+                else if (gameName == "Fornite")
+                {
+                    fornite++;
+                }
+                else if (gameName == "Overwatch")
+                {
+                    overwatch++;
+                }
+                else
+                {
+                    others++;
+                }
+                total = hearthstone + fornite + overwatch + others;
+
+            }
+            Console.WriteLine($"Hearthstone - {hearthstone*1.0/total*100:f2}%");
+            Console.WriteLine($"Fornite - {fornite*1.0/total*100:f2}%");
+            Console.WriteLine($"Overwatch - {overwatch*1.0/total*100:f2}%");
+            Console.WriteLine($"Others - {others*1.0/total*100:f2}%");
+        }
+    }
+}
