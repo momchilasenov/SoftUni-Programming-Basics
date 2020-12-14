2e4afc1d4c604f55e8e0d6cc4e59dd87ec4b7a0c
momchilasenov <momchil276@gmail.com>
2020-12-14 20:26:22 +0200

    Added exam problem

---
+++ 08.ExamPreparation/02.PB-Online-Exam-6-and-7-April/05. Movie Ratings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05._Movie_Ratings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfMovies = int.Parse(Console.ReadLine());
+
+            double maxRating = int.MinValue;
+            string maxMovie = "";
+            double minRating = int.MaxValue;
+            string minMovie = "";
+
+            double ratingSum = 0;
+
+            for (int i = 1; i <= numberOfMovies; i++)
+            {
+                string movieName = Console.ReadLine();
+                double ratingOfMovie = double.Parse(Console.ReadLine());
+                ratingSum += ratingOfMovie;
+
+                if (ratingOfMovie > maxRating)
+                {
+                    maxRating = ratingOfMovie;
+                    maxMovie = movieName;
+                }
+                if (ratingOfMovie < minRating)
+                {
+                    minRating = ratingOfMovie;
+                    minMovie = movieName;
+                }
+            }
+            double averageRating = ratingSum / numberOfMovies;
+
+            Console.WriteLine($"{maxMovie} is with highest rating: {maxRating:f1}");
+            Console.WriteLine($"{minMovie} is with lowest rating: {minRating:f1}");
+            Console.WriteLine($"Average rating: {averageRating:f1}");
+        }
+    }
+}
