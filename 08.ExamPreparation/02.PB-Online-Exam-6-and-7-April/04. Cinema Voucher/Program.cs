5631da2abde23a5b7a6497529faba0756879b73d
momchilasenov <momchil276@gmail.com>
2020-12-14 20:25:52 +0200

    Added exam problem

---
+++ 08.ExamPreparation/02.PB-Online-Exam-6-and-7-April/04. Cinema Voucher/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _04._Cinema_Voucher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int voucherValue = int.Parse(Console.ReadLine());
+            int moviePrice = 0;
+            int productPrice = 0;
+            int purchasedProducts = 0;
+            int purchasedTickets = 0;
+            while (voucherValue >= 0)
+            {
+                string purchase = Console.ReadLine();
+                if (purchase == "End")
+                {
+                    Console.WriteLine($"{purchasedTickets}");
+                    Console.WriteLine($"{purchasedProducts}");
+                    break;
+                }
+                if (purchase.Length > 8)
+                {
+                    moviePrice = purchase[0] + purchase[1];
+                    if (voucherValue < moviePrice)
+                    {
+                        Console.WriteLine($"{purchasedTickets}");
+                        Console.WriteLine($"{purchasedProducts}");
+                        break;
+                    }
+                    voucherValue -= moviePrice;
+                    purchasedTickets++;
+                    
+                }
+                else if(purchase.Length <= 8)
+                {
+                    productPrice = purchase[0];
+                    if (voucherValue < productPrice)
+                    {
+                        Console.WriteLine($"{purchasedTickets}");
+                        Console.WriteLine($"{purchasedProducts}");
+                        break;
+                    }
+                    voucherValue -= productPrice;
+                    purchasedProducts++;
+                    
+                }
+            }
+        }
+    }
+}
