66e2cc9ea953facee08c1f0cdef5c9852aef621d
momchilasenov <momchil276@gmail.com>
2020-12-14 20:20:13 +0200

    Added exam problems

---
+++ 08.ExamPreparation/02.PB-Online-Exam-6-and-7-April/02. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int actors = int.Parse(Console.ReadLine());
+            double priceForCLothes = double.Parse(Console.ReadLine());
+
+            double decor = budget * 0.10;
+            double totalClothes = actors * priceForCLothes;
+            if (actors > 150)
+            {
+                totalClothes -= totalClothes * 0.10;
+            }
+
+            double total = decor + totalClothes;
+
+            if (budget >= total)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget-total:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {total-budget:f2} leva more.");
+            }
+
+        }
+    }
+}
