0bf54ea9f2a8d1860966af818993ea9d44d570c9
momchilasenov <momchil276@gmail.com>
2020-12-14 20:27:01 +0200

    Added exam problem

---
+++ 08.ExamPreparation/02.PB-Online-Exam-6-and-7-April/06. Cinema Tickets/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+
+namespace _06._Cinema_Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine(); //taxi
+
+            bool flag = false;
+            double totalCount = 0;
+            int totalStudentCount = 0;
+            int totalStandardCount = 0;
+            int totalKidCount = 0;
+
+            while (movie != "Finish")
+            {
+                int availableSeats = int.Parse(Console.ReadLine());
+
+                int currentSeats = 0;
+                int studentCount = 0;
+                int standardCount = 0;
+                int kidCount = 0;
+
+                while (true)
+                {
+                    string ticketType = Console.ReadLine();
+                    if (ticketType == "End")
+                    {
+                        Console.WriteLine($"{movie} - {currentSeats*1.0/availableSeats*100:f2}% full.");
+                        break;
+                    }
+
+                    else if (ticketType == "Finish")
+                    {
+                        Console.WriteLine($"{movie} - {currentSeats * 1.0 / availableSeats * 100:f2}% full.");
+                        flag = true;
+                        break;
+                    }
+
+
+                    if (ticketType == "student")
+                    {
+                        studentCount++;
+                        totalStudentCount++;
+                    }
+                    else if (ticketType == "standard")
+                    {
+                        standardCount++;
+                        totalStandardCount++;
+                    }
+                    else if (ticketType == "kid")
+                    {
+                        kidCount++;
+                        totalKidCount++;
+                    }
+                    currentSeats = studentCount + standardCount + kidCount;
+                    totalCount = totalStandardCount + totalStudentCount + totalKidCount;
+                    if (currentSeats == availableSeats)
+                    {
+                        Console.WriteLine($"{movie} - {currentSeats * 1.0 / availableSeats * 100:f2}% full.");
+                        break;
+                    }
+                    
+
+                }
+                if (flag)
+                {
+                    break;
+                }
+                movie = Console.ReadLine();
+            }
+            Console.WriteLine($"Total tickets: {totalCount}");
+            Console.WriteLine($"{totalStudentCount*1.0/totalCount*100:f2}% student tickets.");
+            Console.WriteLine($"{totalStandardCount*1.0/totalCount*100:f2}% standard tickets.");
+            Console.WriteLine($"{totalKidCount*1.0/totalCount*100:f2}% kids tickets.");
+
+        }
+    }
+}
+
