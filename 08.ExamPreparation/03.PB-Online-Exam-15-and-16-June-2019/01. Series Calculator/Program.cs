ff504fdc63b339d719596f3d381270445adb413a
momchilasenov <momchil276@gmail.com>
2020-12-15 22:40:58 +0200

    Added exam problems

---
+++ 08.ExamPreparation/03.PB-Online-Exam-15-and-16-June-2019/01. Series Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Series_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int numberOfSeasons = int.Parse(Console.ReadLine());
+            int numberOfEpisodes = int.Parse(Console.ReadLine());
+            double episodeDuration = double.Parse(Console.ReadLine());
+
+            double commercials = episodeDuration * 0.2;
+
+            double totalDuration = episodeDuration + commercials;
+
+            double specialEpisodes = numberOfSeasons * 10;
+
+            double totalTimeForSeries = totalDuration * numberOfSeasons * numberOfEpisodes + specialEpisodes;
+
+            Console.WriteLine($"Total time needed to watch the {seriesName} series is {totalTimeForSeries} minutes.");
+        }
+    }
+}
---
+++ 08.ExamPreparation/03.PB-Online-Exam-15-and-16-June-2019/02. Lunch Break/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Lunch_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int episodeDuration = int.Parse(Console.ReadLine());
+            int breakDuration = int.Parse(Console.ReadLine());
+
+            double lunchTime = breakDuration * 1* 1*1.0 / 8;
+            double restTime = breakDuration * 1 * 1.0 / 4;
+            double timeLeft = breakDuration - lunchTime - restTime; 
+
+            if (timeLeft >= episodeDuration)
+            {
+                Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(timeLeft - episodeDuration)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(episodeDuration-timeLeft)} more minutes.");
+            }
+        }
+    }
+}
