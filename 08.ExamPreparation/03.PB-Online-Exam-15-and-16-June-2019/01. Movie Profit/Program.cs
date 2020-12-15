24c9f29fd475d2b7ada7f1539da5f51af95fb4bc
momchilasenov <momchil276@gmail.com>
2020-12-15 22:40:00 +0200

    Added exam problems

---
+++ 08.ExamPreparation/03.PB-Online-Exam-15-and-16-June-2019/01. Movie Profit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Movie_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int tickets = int.Parse(Console.ReadLine());
+            double pricePerTicket = double.Parse(Console.ReadLine());
+            int percentageForCinema = int.Parse(Console.ReadLine());
+
+            double profitPerDay = tickets * pricePerTicket;
+            double totalProfit = days * profitPerDay;
+            totalProfit -= totalProfit * percentageForCinema*1.0/100;
+
+            Console.WriteLine($"The profit from the movie {movieName} is {totalProfit:f2} lv.");
+        }
+    }
+}
