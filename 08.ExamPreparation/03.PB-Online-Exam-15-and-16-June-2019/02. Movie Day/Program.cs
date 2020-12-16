33c372582b26e7ef9cccf9c4781eb1772191c4b9
momchilasenov <momchil276@gmail.com>
2020-12-16 22:57:59 +0200

    Added exam problems

---
+++ 08.ExamPreparation/03.PB-Online-Exam-15-and-16-June-2019/02. Movie Day/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02._Movie_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeForAction = int.Parse(Console.ReadLine());
+            int numberOfScenes = int.Parse(Console.ReadLine());
+            int sceneDuration = int.Parse(Console.ReadLine());
+
+            double prep = 0.15 * timeForAction;
+
+            double timeForShooting = numberOfScenes * sceneDuration;
+            double totalTime = timeForShooting + prep; 
+
+            if (totalTime <= timeForAction)
+            {
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeForAction - totalTime)} minutes left!");
+            }
+            else
+            {
+                Console.WriteLine($"Time is up! To complete the movie you need {totalTime - timeForAction} minutes.");
+            }
+
+        }
+    }
+}
