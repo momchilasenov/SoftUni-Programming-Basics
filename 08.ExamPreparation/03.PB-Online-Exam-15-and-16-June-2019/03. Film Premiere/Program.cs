70af8ca9d1424f81be4ff2d9b98a6aceb8a65183
momchilasenov <momchil276@gmail.com>
2020-12-16 22:58:48 +0200

    Added exam problem

---
+++ 08.ExamPreparation/03.PB-Online-Exam-15-and-16-June-2019/03. Film Premiere/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+
+namespace _03._Film_Premiere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            string menu = Console.ReadLine();
+            int numberOfTickets = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (menu == "Drink")
+            {
+                if (movie == "John Wick")
+                {
+                    price = 12;
+                }
+                else if (movie == "Star Wars")
+                {
+                    price = 18;
+                }
+                else if (movie == "Jumanji")
+                {
+                    price = 9;
+                }
+            }
+            else if (menu == "Popcorn")
+            {
+                if (movie == "John Wick")
+                {
+                    price = 15;
+                }
+                else if (movie == "Star Wars")
+                {
+                    price = 25;
+                }
+                else if (movie == "Jumanji")
+                {
+                    price = 11;
+                }
+            }
+            else if (menu == "Menu")
+            {
+                if (movie == "John Wick")
+                {
+                    price = 19;
+                }
+                else if (movie == "Star Wars")
+                {
+                    price = 30;
+                }
+                else if (movie == "Jumanji")
+                {
+                    price = 14;
+                }
+            }
+
+            double totalPrice = price * numberOfTickets;
+
+            if (movie == "Star Wars" && numberOfTickets >= 4)
+            {
+                totalPrice -= totalPrice * 0.3;
+            }
+            else if (movie == "Jumanji" && numberOfTickets == 2)
+            {
+                totalPrice -= totalPrice * 0.15;
+            }
+
+            Console.WriteLine($"Your bill is {totalPrice:f2} leva.");
+
+        }
+    }
+}
