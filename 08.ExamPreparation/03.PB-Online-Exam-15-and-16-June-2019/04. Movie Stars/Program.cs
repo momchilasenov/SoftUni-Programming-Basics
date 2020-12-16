d801a3c99a4e951175034ce61768df3ed0a95f87
momchilasenov <momchil276@gmail.com>
2020-12-16 23:02:37 +0200

    Added exam problem

---
+++ 08.ExamPreparation/03.PB-Online-Exam-15-and-16-June-2019/04. Movie Stars/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _04._Movie_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+
+            double payment = 0;
+
+            while (true)
+            {
+                string actor = Console.ReadLine();
+                if (actor == "ACTION")
+                {
+                    Console.WriteLine($"We are left with {budget:f2} leva.");
+                    break;
+                }
+
+                if (actor.Length <= 15)
+                {
+                    payment = double.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    payment = budget * 0.2;
+                }
+
+                if (payment > budget)
+                {
+                    Console.WriteLine($"We need {Math.Abs(budget - payment):f2} leva for our actors.");
+                    break;
+                }
+                budget -= payment;
+            }
+
+        }
+    }
+}
---
+++ 08.ExamPreparation/03.PB-Online-Exam-15-and-16-June-2019/05. Oscars/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05._Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double academyPoints = double.Parse(Console.ReadLine());
+            int judges = int.Parse(Console.ReadLine());
+
+            double score = 0;
+
+            for (int i = 1; i<= judges; i++)
+            {
+                string nameOfJudge = Console.ReadLine();
+                double pointsOfJudge = double.Parse(Console.ReadLine());
+
+                score = (nameOfJudge.Length * pointsOfJudge) / 2;
+
+                academyPoints += score;
+                if (academyPoints > 1250.5)
+                {
+                    break;
+                }
+                                
+            }
+            if (academyPoints > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {name} got a nominee for leading role with {academyPoints:f1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {name} you need {1250.5 - academyPoints:f1} more!");
+            }
+        }
+    }
+}
