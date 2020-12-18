a99544d1670a7867d6c4efd3678c5f307dd7e916
momchilasenov <momchil276@gmail.com>
2020-12-18 21:17:09 +0200

    Added exam problems

---
+++ 08.ExamPreparation/04. PB Online Exam - 9 and 10 March 2019/01. Basketball Equipment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearlyFeeForBasketball = int.Parse(Console.ReadLine());
+
+            double sneakers = yearlyFeeForBasketball * 0.6;
+            double clothing = sneakers - sneakers * 0.2;
+            double ball = clothing * 0.25;
+            double accessories = ball * 0.2;
+
+            double total = yearlyFeeForBasketball + sneakers + clothing + ball + accessories;
+
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
