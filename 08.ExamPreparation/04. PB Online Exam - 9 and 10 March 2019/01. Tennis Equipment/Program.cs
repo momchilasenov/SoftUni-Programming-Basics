3c5b8a3eb9a93712f3fd9ef8d3b3b5b0b85d9782
momchilasenov <momchil276@gmail.com>
2020-12-18 21:18:12 +0200

    Added exam problem

---
+++ 08.ExamPreparation/04. PB Online Exam - 9 and 10 March 2019/01. Tennis Equipment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Tennis_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tennisRacketPrice = double.Parse(Console.ReadLine());
+            int numberOfRackets = int.Parse(Console.ReadLine());
+            int sneakerPairs = int.Parse(Console.ReadLine());
+
+            double totalRackets = numberOfRackets * tennisRacketPrice;
+            double totalSneakers = sneakerPairs * tennisRacketPrice * 1 / 6;
+            double accessories = (totalRackets + totalSneakers) * 0.2;
+            double total = totalRackets + totalSneakers + accessories;
+
+            double priceForDjokovic = total * 1 / 8;
+            double priceForSponsors = total * 7 / 8;
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(priceForDjokovic)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(priceForSponsors)}");
+
+        }
+    }
+}
