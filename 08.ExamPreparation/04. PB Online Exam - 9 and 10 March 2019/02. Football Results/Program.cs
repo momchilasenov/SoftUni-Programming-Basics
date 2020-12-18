0600854cd5a2603ff77d34cad11296fea7491245
momchilasenov <momchil276@gmail.com>
2020-12-18 21:19:28 +0200

    Added exam problem

---
+++ 08.ExamPreparation/04. PB Online Exam - 9 and 10 March 2019/02. Football Results/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _02._Football_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string firstMatch = Console.ReadLine();
+            //string secondMatch = Console.ReadLine();
+            //string thirdMatch = Console.ReadLine();
+
+            int wonGames = 0;
+            int lostGames = 0;
+            int drawnGames = 0;
+
+            for (int i= 1; i<=3; i++)
+            {
+                string match = Console.ReadLine();
+                int first = int.Parse(match[0].ToString());
+                int second = int.Parse(match[2].ToString());
+                
+                if (first > second)
+                {
+                    wonGames++;
+                }
+                else if (first < second)
+                {
+                    lostGames++;
+                }
+                else if (first == second)
+                {
+                    drawnGames++;
+                }
+
+            }
+            Console.WriteLine($"Team won {wonGames} games.");
+            Console.WriteLine($"Team lost {lostGames} games.");
+            Console.WriteLine($"Drawn games: {drawnGames}");
+
+            //char number = firstMatch[0];
+            //int result1 = int.Parse(number.ToString());
+            //Console.WriteLine(goal1);
+
+            //char number2 = firstMatch[2];
+            //int result2 = int.Parse(number2.ToString());
+
+
+        }
+    }
+}
