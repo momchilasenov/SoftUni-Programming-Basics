8487aa6af3dfd524dfc047e9e18fa4b3eaf70e53
momchilasenov <momchil276@gmail.com>
2020-12-19 08:44:13 +0200

    Added exam problem

---
+++ 08.ExamPreparation/04. PB Online Exam - 9 and 10 March 2019/02. Skeleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02._Skeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+            double lengthOfTunnel = double.Parse(Console.ReadLine());
+            int timePer100Meters = int.Parse(Console.ReadLine());
+
+            double timeToBeat = minutes * 60 + seconds;
+            double timeInTunnel = lengthOfTunnel / 100;
+            double time = timePer100Meters * timeInTunnel;
+
+            double delay = (lengthOfTunnel / 120) * 2.5;
+            double finalTime = time - delay;
+
+            if (finalTime <= timeToBeat)
+            {
+                Console.WriteLine("Marin Bangiev won an Olympic quota!");
+                Console.WriteLine($"His time is {finalTime:f3}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, Marin failed! He was {finalTime - timeToBeat:f3} second slower.");
+            }
+
+        }
+    }
+}
