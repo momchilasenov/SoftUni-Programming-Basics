b5d15c9bb1cf31f71a9e39ea2e5fc04a857d7cd1
momchilasenov <momchil276@gmail.com>
2020-12-19 08:52:03 +0200

    Added exam problem

---
+++ 08.ExamPreparation/04. PB Online Exam - 9 and 10 March 2019/04. Darts/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+
+namespace _04._Darts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string player = Console.ReadLine();
+            double totalPoints = 301;
+            int successfulShots = 0;
+            int unsuccessfulShots = 0;
+            int pointsPerShot = 0;
+
+            string command = Console.ReadLine();
+
+
+            while (command != "Retire")
+            {
+                int points = int.Parse(Console.ReadLine()); 
+
+                if (command == "Triple")
+                {
+                    pointsPerShot = points * 3;
+                    totalPoints -= pointsPerShot;
+                    successfulShots++;
+                }
+                else if (command == "Double")
+                {
+                    pointsPerShot = points * 2;
+                    totalPoints -= pointsPerShot;
+                    successfulShots++;
+                }
+                else if (command == "Single")
+                {
+                    pointsPerShot = points;
+                    totalPoints -= pointsPerShot;
+                    successfulShots++; //10 - 13 = -3 => 
+                }
+
+                if (totalPoints < 0)
+                {
+                    successfulShots--;
+                    unsuccessfulShots++;
+                    totalPoints = totalPoints + pointsPerShot;
+                }
+
+                if (totalPoints == 0)
+                {
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+            if (command == "Retire")
+            {
+                Console.WriteLine($"{player} retired after {unsuccessfulShots} unsuccessful shots.");
+            }
+            else
+            {
+                Console.WriteLine($"{player} won the leg with {successfulShots} shots.");
+            }
+            
+        }
+    }
+}
