86c454852f03e219b8102b80465748afc93e2656
momchilasenov <momchil276@gmail.com>
2020-12-20 20:27:28 +0200

    Added exam problem

---
+++ 08.ExamPreparation/04. PB Online Exam - 9 and 10 March 2019/06. High Jump/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+
+namespace _06._High_Jump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int desiredJump = int.Parse(Console.ReadLine());
+
+            int failedJumps = 0;
+            int currentGoal = desiredJump - 30;
+            int jumpCounter = 0;
+            int currentJump = 0; 
+
+            while (true) 
+            {
+                currentJump = int.Parse(Console.ReadLine()); 
+                jumpCounter++;                               
+
+                failedJumps = 0;
+
+                while (currentJump <= currentGoal) 
+                { 
+                    failedJumps++;
+
+                    if (failedJumps == 3) 
+                    {
+                        Console.WriteLine($"Tihomir failed at {currentGoal}cm after {jumpCounter} jumps.");
+                        return;
+                    }
+
+                    currentJump = int.Parse(Console.ReadLine()); 
+                    jumpCounter++; 
+
+                }
+
+                if (currentJump > currentGoal) 
+                {
+                    if (currentGoal >= desiredJump) 
+                    {
+                        break; 
+                    }
+                    currentGoal += 5; 
+
+                }
+
+            }
+            if (failedJumps < 3) 
+            {
+                Console.WriteLine($"Tihomir succeeded, he jumped over {currentGoal}cm after {jumpCounter} jumps.");
+            }
+
+        }
+    }
+}
+
+
