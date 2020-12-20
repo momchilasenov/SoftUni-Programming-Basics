7adbb38392023351ec1d27a3426803c5575b0e49
momchilasenov <momchil276@gmail.com>
2020-12-20 20:24:11 +0200

    Added exam problem

---
+++ 08.ExamPreparation/04. PB Online Exam - 9 and 10 March 2019/05. Tennis Ranklist/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _05._Tennis_Ranklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTournaments = int.Parse(Console.ReadLine());
+            int startingPoints = int.Parse(Console.ReadLine());
+
+            double pointsWon = 0;
+            double wonTournaments = 0;
+
+            for (int i = 1; i <= numberOfTournaments; i++)
+            {
+                string stageReached = Console.ReadLine();
+
+                if (stageReached == "W")
+                {
+                    startingPoints += 2000;
+                    pointsWon += 2000;
+                    wonTournaments++;
+                }
+                else if (stageReached == "F")
+                {
+                    startingPoints += 1200;
+                    pointsWon += 1200;
+                }
+                else if (stageReached == "SF")
+                {
+                    startingPoints += 720;
+                    pointsWon += 720;
+                }
+
+            }
+            double averagePoints = Math.Floor(pointsWon / numberOfTournaments);
+
+            Console.WriteLine($"Final points: {startingPoints}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{(wonTournaments / numberOfTournaments) * 100:f2}%");
+
+
+
+
+        }
+    }
+}
