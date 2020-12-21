0ddc21789bea359a90f602b6777f66a6842d93ff
momchilasenov <momchil276@gmail.com>
2020-12-21 18:08:29 +0200

    Added exam problem

---
+++ 08.ExamPreparation/05.PB-Online-Retake-Exam-2-and-3-May-2019/05. Division Without Remainder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05._Division_Without_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int dividesBy2 = 0;
+            int dividesBy3 = 0;
+            int dividesBy4 = 0;
+
+            for (int i = 1; i<=numbers; i++)
+            {
+                int newNumber = int.Parse(Console.ReadLine());
+
+                if (newNumber % 2 == 0)
+                {
+                    dividesBy2++;
+                }
+                if (newNumber % 3 == 0)
+                {
+                    dividesBy3++;
+                }
+                if (newNumber % 4 == 0)
+                {
+                    dividesBy4++;
+                }
+            }
+            double percentage2 = dividesBy2*1.0 / numbers * 100;
+            double percentage3 = dividesBy3*1.0 / numbers * 100;
+            double percentage4 = dividesBy4*1.0 / numbers * 100;
+
+            Console.WriteLine($"{percentage2:f2}%");
+            Console.WriteLine($"{percentage3:f2}%");
+            Console.WriteLine($"{percentage4:f2}%");
+
+        }
+    }
+}
