41896c5f80462b549e0ac902e8e2104dea458521
momchilasenov <momchil276@gmail.com>
2020-12-21 17:46:19 +0200

    Added exam problem

---
+++ 08.ExamPreparation/05.PB-Online-Retake-Exam-2-and-3-May-2019/01. Food Delivery/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int veggieMenus = int.Parse(Console.ReadLine());
+
+            double chickenPrice = 10.35;
+            double fishPrice = 12.40;
+            double veggiePrice = 8.15;
+
+            double total = chickenMenus * chickenPrice + fishMenus * fishPrice + veggieMenus * veggiePrice;
+
+            double desert = total * 0.2;
+
+            double totalPrice = total + desert + 2.50;
+            Console.WriteLine($"Total: {totalPrice:f2}");
+
+
+        }
+    }
+}
