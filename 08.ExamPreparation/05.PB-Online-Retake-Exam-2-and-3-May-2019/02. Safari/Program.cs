dbc2c0634c773dc8bbd1be76682bdb66cf815409
momchilasenov <momchil276@gmail.com>
2020-12-21 17:46:58 +0200

    Added exam problem

---
+++ 08.ExamPreparation/05.PB-Online-Retake-Exam-2-and-3-May-2019/02. Safari/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02._Safari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double letersNeeded = double.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+
+            double gasPrice = letersNeeded * 2.10;
+            double totalCost = gasPrice + 100;
+
+            double discount = 0;
+
+            if (dayOfWeek == "Saturday")
+            {
+                discount = 0.1;
+            }
+            else if (dayOfWeek == "Sunday")
+            {
+                discount = 0.2;
+            }
+
+            totalCost -= totalCost * discount;
+
+            if (budget >= totalCost)
+            {
+                Console.WriteLine($"Safari time! Money left: {budget - totalCost:f2} lv. ");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! Money needed: {totalCost-budget:f2} lv.");
+            }
+        }
+    }
+}
