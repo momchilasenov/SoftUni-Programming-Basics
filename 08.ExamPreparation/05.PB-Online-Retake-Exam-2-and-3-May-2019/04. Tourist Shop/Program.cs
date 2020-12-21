f32eb6ac8971418df06e59f5bef09ec334ac3848
momchilasenov <momchil276@gmail.com>
2020-12-21 18:07:53 +0200

    Added exam problem

---
+++ 08.ExamPreparation/05.PB-Online-Retake-Exam-2-and-3-May-2019/04. Tourist Shop/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+
+namespace _04._Tourist_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double budget = double.Parse(Console.ReadLine());
+           
+            double price = 0;
+            double totalPrice = 0;
+            int productCounter = 0;
+
+            while (true)
+            {
+                string product = Console.ReadLine();
+
+                if (product == "Stop")
+                {
+                    break;
+                }
+                productCounter++;
+
+                price = double.Parse(Console.ReadLine());
+                if (productCounter % 3 == 0 && productCounter != 0)
+                {
+                    price *= 0.5;
+                }
+
+                totalPrice += price;
+
+
+                if (price > budget)
+                {
+                    Console.WriteLine("You don't have enough money!");
+                    Console.WriteLine($"You need {price - budget:f2} leva!");
+                    break;
+                }
+                budget -= price;
+
+
+            }
+            if (budget >= price)
+            {
+                Console.WriteLine($"You bought {productCounter} products for {totalPrice:f2} leva.");
+
+            }
+        }
+    }
+}
