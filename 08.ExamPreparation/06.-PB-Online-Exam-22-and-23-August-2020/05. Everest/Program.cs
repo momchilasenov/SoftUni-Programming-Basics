4f2e45cdbbc73f84976f37362397a9ccf282d564
momchilasenov <momchil276@gmail.com>
2020-12-22 21:07:13 +0200

    Added exam problem

---
+++ 08.ExamPreparation/06.-PB-Online-Exam-22-and-23-August-2020/05. Everest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _05._Everest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string restOrNo = Console.ReadLine();
+
+            double heightReached = 5364;
+
+            int dayCounter = 1;
+
+            while (restOrNo != "END")
+            {
+                if (restOrNo == "Yes")
+                {
+                    dayCounter++;
+                }
+                if (dayCounter > 5)
+                {
+                    
+                    break;
+                }
+
+                int metersClimbed = int.Parse(Console.ReadLine());
+                heightReached += metersClimbed;
+                if (heightReached >= 8848)
+                {
+                    break;
+                }
+
+                restOrNo = Console.ReadLine();
+            }
+            if (heightReached >= 8848)
+            {
+                Console.WriteLine($"Goal reached for {dayCounter} days!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine($"{heightReached}");
+            }
+        }
+    }
+}
