b490a7f7cd08671fac11154bef8430b52f6678a6
momchilasenov <momchil276@gmail.com>
2020-12-22 21:05:00 +0200

    Added exam problem

---
+++ 08.ExamPreparation/06.-PB-Online-Exam-22-and-23-August-2020/02. Illidan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Illidan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInGroup = int.Parse(Console.ReadLine());
+            int strengthPerPerson = int.Parse(Console.ReadLine());
+            int bloodOfIllidian = int.Parse(Console.ReadLine());
+
+            double totalStrength = peopleInGroup * strengthPerPerson;
+
+            if (totalStrength >= bloodOfIllidian)
+            {
+                Console.WriteLine($"Illidan has been slain! You defeated him with {totalStrength-bloodOfIllidian} points.");
+            }
+            else
+            {
+                Console.WriteLine($"You are not prepared! You need {bloodOfIllidian-totalStrength} more points.");
+            }
+        }
+    }
+}
