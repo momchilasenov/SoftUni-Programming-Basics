279350267c4d270927aa43a0ba395aadf6087b81
momchilasenov <momchil276@gmail.com>
2020-12-22 21:06:42 +0200

    Added exam problem

---
+++ 08.ExamPreparation/06.-PB-Online-Exam-22-and-23-August-2020/04. Rate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Numerics;
+
+namespace _04._Rate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialDeposit = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+
+            double simpleRate = 0;
+            double complexRate = 0;
+
+            double simpleDeposit = initialDeposit;
+            double complexDeposit = initialDeposit;
+            string chosenInterest = "";
+
+            for (int i = 1; i <= months; i++)
+            {
+
+                simpleRate = simpleDeposit + 0.03 * initialDeposit;
+                complexRate = complexDeposit + 0.027 * complexDeposit;
+
+                simpleDeposit = simpleRate;
+                complexDeposit = complexRate;
+
+            }
+            
+            if (simpleRate > complexRate)
+            {
+                chosenInterest = "simple";
+            }
+            else if (simpleRate < complexRate)
+            {
+                chosenInterest = "complex";
+            }
+
+            double difference = Math.Abs(simpleRate - complexRate);
+
+            Console.WriteLine($"Simple interest rate: {simpleRate:f2} lv.");
+            Console.WriteLine($"Complex interest rate: {complexRate:f2} lv.");
+            Console.WriteLine($"Choose a {chosenInterest} interest rate. You will win {difference:f2} lv.");
+        }
+    }
+}
