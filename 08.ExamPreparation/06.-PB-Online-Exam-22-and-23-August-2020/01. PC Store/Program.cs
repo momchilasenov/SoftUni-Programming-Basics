ef974aa0e0ee400376af6b750483f783b8df1add
momchilasenov <momchil276@gmail.com>
2020-12-22 21:09:33 +0200

    Added exam problem

---
+++ 08.ExamPreparation/06.-PB-Online-Exam-22-and-23-August-2020/01. PC Store/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cpuPrice = double.Parse(Console.ReadLine());
+            double gpuPrice = double.Parse(Console.ReadLine());
+            double ramPrice = double.Parse(Console.ReadLine());
+            int numberOfRams = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            double cpuPriceInLeva = cpuPrice * 1.57;
+            double gpuPriceInLeva = gpuPrice * 1.57;
+            double ramPriceInLeva = ramPrice * 1.57;
+            double totalForRam = ramPriceInLeva * numberOfRams;
+            cpuPriceInLeva -= cpuPriceInLeva * discount;
+            gpuPriceInLeva -= gpuPriceInLeva * discount;
+
+            double total = cpuPriceInLeva + gpuPriceInLeva + totalForRam;
+
+            Console.WriteLine($"Money needed - {total:f2} leva.");
+
+            //switch (timeOfDay)
+            //{
+            //    case "day":
+            //        switch (month)
+            //        {
+            //            case "march":
+            //            case "april":
+            //            case "may":
+            //                price = 10.5;
+            //                break;
+            //            case "june":
+            //            case "july":
+            //            case "august":
+            //                price = 12.60;
+            //                break;
+            //        }
+            //        break;
+            //    case "night":
+            //        switch (month)
+            //        {
+            //            case "march":
+            //            case "april":
+            //            case "may":
+            //                price = 8.4;
+            //                break;
+            //            case "june":
+            //            case "july":
+            //            case "august":
+            //                price = 10.2;
+            //                break;
+            //        }
+            //        break;
+            //}
+
+        }
+    }
+}
