3f86298762931448438312fe22042cd96c349b21
momchilasenov <momchil276@gmail.com>
2020-12-23 17:33:26 +0200

    Added exam problem

---
+++ 08.ExamPreparation/07.PB-Exam/04. Darts Tournament/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+
+namespace _04._Darts_Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int startingPoints = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            while (true)
+            { 
+                string sector = Console.ReadLine();
+                counter++; //след като уцели сектор броим удар
+
+                if (sector == "bullseye")
+                {
+                    Console.WriteLine($"Congratulations! You won the game with a bullseye in {counter} moves!");
+                    break;
+                }
+
+                int points = int.Parse(Console.ReadLine());
+
+                if (sector == "number section")
+                {
+                    startingPoints -= points;
+                }
+                else if (sector == "double ring")
+                {
+                    startingPoints -= 2 * points;
+                }
+                else if (sector == "triple ring")
+                {
+                    startingPoints -= 3 * points;
+                }
+
+                if (startingPoints == 0)
+                {
+                    Console.WriteLine($"Congratulations! You won the game in {counter} moves!");
+                    break; // задължително break-ваш цикъла
+                }
+                else if (startingPoints < 0)
+                {
+                    Console.WriteLine($"Sorry, you lost. Score difference: {Math.Abs(startingPoints)}.");
+                    break;
+                }
+
+            }
+
+
+
+        }
+    }
+}
