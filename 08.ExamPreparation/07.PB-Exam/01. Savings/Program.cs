38ec04ed037305ff23785e8c0f0a392bb9f7d412
momchilasenov <momchil276@gmail.com>
2020-12-23 17:29:53 +0200

    Added exam problem

---
+++ 08.ExamPreparation/07.PB-Exam/01. Savings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Savings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double incomeDesi = double.Parse(Console.ReadLine());
+            int monthsToSave = int.Parse(Console.ReadLine());
+            double personalExpenses = double.Parse(Console.ReadLine());
+
+            double unpredictedExpenses = incomeDesi * 0.3;
+
+            double moneySaved = incomeDesi - unpredictedExpenses - personalExpenses;
+            double totalMoneySaved = monthsToSave * moneySaved;
+
+            Console.WriteLine($"She can save {moneySaved/incomeDesi * 100:f2}%");
+            Console.WriteLine($"{totalMoneySaved:f2}");
+        }
+    }
+}
