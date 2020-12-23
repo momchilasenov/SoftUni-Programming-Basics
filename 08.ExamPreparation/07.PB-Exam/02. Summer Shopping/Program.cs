80bba2514fccce1886617556f77ad9904bed722f
momchilasenov <momchil276@gmail.com>
2020-12-23 17:31:02 +0200

    Added exam problem

---
+++ 08.ExamPreparation/07.PB-Exam/02. Summer Shopping/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Summer_Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            double beachTowel = double.Parse(Console.ReadLine());
+            int discountInPercentage = int.Parse(Console.ReadLine());
+
+            double umbrellaPrice = beachTowel * 2.0 / 3; 
+            double flipFlops = umbrellaPrice * 0.75;
+            double beachBag = ((beachTowel + flipFlops)/ 3);
+
+            double total = umbrellaPrice + flipFlops + beachTowel + beachBag;
+
+            total -= total * discountInPercentage/100;
+
+            if (total <= budget)
+            {
+                Console.WriteLine($"Annie's sum is {total:f2} lv. She has {budget-total:f2} lv. left.");
+            }
+            else if (total> budget)
+            {
+                Console.WriteLine($"Annie's sum is {total:f2} lv. She needs {total-budget:f2} lv. more.");
+            }
+        }
+    }
+}
