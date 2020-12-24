315c5a80a380688408bb07e19db18010547700b8
momchilasenov <momchil276@gmail.com>
2020-12-24 23:09:56 +0200

    Added exam problem

---
+++ 08.ExamPreparation/08.PB-Online-Exam-20-and-21-April-2019/01. Easter Lunch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace _01._Easter_Lunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int easterBreads = int.Parse(Console.ReadLine());
+            int eggCrates = int.Parse(Console.ReadLine());
+            int cookiesInKg = int.Parse(Console.ReadLine());
+
+            double totalEasterBreads = easterBreads * 3.20;
+            double totalEggs = eggCrates * 4.35;
+            double totalCookies = cookiesInKg * 5.40;
+            double paintForEggs = eggCrates * 12 * 0.15;
+
+            double total = totalEasterBreads + totalCookies + totalEggs + paintForEggs;
+            Console.WriteLine($"{total:f2}");
+
+        }
+    }
+}
