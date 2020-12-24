c19cf242d46f63f532f728455e01a0eeb6ac4ef8
momchilasenov <momchil276@gmail.com>
2020-12-24 23:09:13 +0200

    Added exam problem

---
+++ 08.ExamPreparation/08.PB-Online-Exam-20-and-21-April-2019/01. Easter Bakery/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Easter_Bakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sugarPricePerKilo = 0;
+            double eggCratePrice = 0;
+            double yeastPrice = 0;
+
+            double flourPricePerKilo = double.Parse(Console.ReadLine());
+            double flourKilos = double.Parse(Console.ReadLine());
+            double sugarKilos = double.Parse(Console.ReadLine());
+            int eggCrates = int.Parse(Console.ReadLine());
+            int yeastPackets = int.Parse(Console.ReadLine());
+
+            sugarPricePerKilo = flourPricePerKilo * 0.75;
+            eggCratePrice = flourPricePerKilo * 1.1;
+            yeastPrice = sugarPricePerKilo * 0.2;
+
+            double flourSum = flourPricePerKilo * flourKilos;
+            double sugarSum = sugarKilos * sugarPricePerKilo;
+            double eggSum = eggCratePrice * eggCrates;
+            double yeastSum = yeastPrice * yeastPackets;
+
+            double total = flourSum + sugarSum + eggSum + yeastSum;
+
+            Console.WriteLine($"{total:f2}");
+
+        }
+    }
+}
