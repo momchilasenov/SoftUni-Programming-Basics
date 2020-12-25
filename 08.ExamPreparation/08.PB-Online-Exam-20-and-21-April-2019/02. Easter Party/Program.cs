f177354da8b244545c64cff69709fcc36edb493a
momchilasenov <momchil276@gmail.com>
2020-12-25 09:33:23 +0200

    Added exam problem

---
+++ 08.ExamPreparation/08.PB-Online-Exam-20-and-21-April-2019/02. Easter Party/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._Easter_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guests = int.Parse(Console.ReadLine());
+            double pricePerPerson = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            if (guests >= 10 && guests <= 15)
+            {
+                pricePerPerson -= pricePerPerson * 0.15;
+            }
+            else if (guests > 15 && guests <= 20)
+            {
+                pricePerPerson -= pricePerPerson * 0.2;
+            }
+            else if (guests > 20)
+            {
+                pricePerPerson -= pricePerPerson * 0.25; 
+            }
+
+            double cakePrice = budget * 0.1;
+            double totalForFood = guests * pricePerPerson;
+            double total = totalForFood + cakePrice;
+
+            if (total <= budget)
+            {
+                Console.WriteLine($"It is party time! {budget-total:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"No party! {total-budget:f2} leva needed.");
+            }
+
+        }
+    }
+}
