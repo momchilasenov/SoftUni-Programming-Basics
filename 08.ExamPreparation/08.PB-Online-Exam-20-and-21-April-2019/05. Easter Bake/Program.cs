1a3ec1370db28c419edac0470214fd40f01e6c5e
momchilasenov <momchil276@gmail.com>
2020-12-26 08:27:08 +0200

    Added exam problem

---
+++ 08.ExamPreparation/08.PB-Online-Exam-20-and-21-April-2019/05. Easter Bake/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _05._Easter_Bake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfEasterBreads = int.Parse(Console.ReadLine());
+            double totalSugar = 0;
+            double totalFlour = 0;
+
+            double maxSugar = 0;
+            double maxFlour = 0;
+
+            for (int i = 1; i <= numberOfEasterBreads; i++)
+            {
+                int sugarUsed = int.Parse(Console.ReadLine());
+                int flourUsed = int.Parse(Console.ReadLine());
+
+                if (maxSugar < sugarUsed)
+                {
+                    maxSugar = sugarUsed;
+                }
+                if (maxFlour < flourUsed)
+                {
+                    maxFlour = flourUsed;
+                }
+
+                totalSugar += sugarUsed;
+                totalFlour += flourUsed;
+            }
+            double packetsOfSugar = Math.Ceiling (totalSugar / 950); 
+            double packetOfFlour = Math.Ceiling (totalFlour / 750);
+
+            Console.WriteLine($"Sugar: {packetsOfSugar}");
+            Console.WriteLine($"Flour: {packetOfFlour}");
+            Console.WriteLine($"Max used flour is {maxFlour} grams, max used sugar is {maxSugar} grams.");
+        }
+    }
+}
