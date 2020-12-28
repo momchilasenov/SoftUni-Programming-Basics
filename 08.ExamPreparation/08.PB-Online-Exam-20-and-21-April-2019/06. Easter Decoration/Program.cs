9d2691c750125b12d374d8178369fa7abc0abcd7
momchilasenov <momchil276@gmail.com>
2020-12-28 22:00:52 +0200

    Added exam problem

---
+++ 08.ExamPreparation/08.PB-Online-Exam-20-and-21-April-2019/06. Easter Decoration/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06._Easter_Decoration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int clientsInStore = int.Parse(Console.ReadLine());
+            double totalMoneyMade = 0;
+
+            for (int i = 1; i <= clientsInStore; i++)
+            {
+                string command = Console.ReadLine();
+                int itemCounter = 0; 
+                double clientTotal = 0; 
+
+                while (command != "Finish")
+                {
+                    itemCounter++;
+                    if (command == "basket")
+                    {
+                        clientTotal += 1.50;
+                    }
+                    else if (command == "wreath")
+                    {
+                        clientTotal += 3.80;
+                    }
+                    else if (command == "chocolate bunny")
+                    {
+                        clientTotal += 7;
+                    }
+
+                    command = Console.ReadLine();
+                }
+                if (itemCounter%2 == 0)
+                {
+                    clientTotal -= clientTotal * 0.2;
+                }
+                Console.WriteLine($"You purchased {itemCounter} items for {clientTotal:f2} leva.");
+                totalMoneyMade += clientTotal;
+            }
+            Console.WriteLine($"Average bill per client is: {totalMoneyMade/clientsInStore:f2} leva.");
+
+
+        }
+    }
+}
