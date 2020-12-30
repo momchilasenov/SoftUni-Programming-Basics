230137ab3cd4bc3cb301615f2ee4c2e9e80a17e4
momchilasenov <momchil276@gmail.com>
2020-12-30 19:46:05 +0200

    Added exam problem

---
+++ 08.ExamPreparation/09.PB-Online-Exam-28-and-29-March-2020/02. Mountain Run/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02._Mountain_Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double totalTime = timePerMeter * distanceInMeters;
+
+            double timesDelayed = Math.Floor(distanceInMeters / 50);
+            double totalDelay = timesDelayed * 30;
+
+            double finalTime = totalTime + totalDelay;
+
+            if (finalTime < record)
+            {
+                Console.WriteLine($"Yes! The new record is {finalTime:f2} seconds.");
+            }
+            else
+            {
+                double difference = finalTime - record;
+                Console.WriteLine($"No! He was {difference:f2} seconds slower.");
+            }
+
+        }
+    }
+}
