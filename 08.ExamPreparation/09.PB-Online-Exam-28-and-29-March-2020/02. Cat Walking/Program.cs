1b373bfb85bb3d9e697438eb95e5578a35524eeb
momchilasenov <momchil276@gmail.com>
2020-12-30 19:45:09 +0200

    Added exam problem

---
+++ 08.ExamPreparation/09.PB-Online-Exam-28-and-29-March-2020/02. Cat Walking/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Cat_Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Input 
+            int walkMinutes = int.Parse(Console.ReadLine());
+            int numberOfWalks = int.Parse(Console.ReadLine());
+            int caloriesGained = int.Parse(Console.ReadLine());
+
+            int totalWalkTime = walkMinutes * numberOfWalks;
+            int caloriesBurned = totalWalkTime * 5;
+
+            double enoughCalories = caloriesGained * 0.5;
+
+            if (caloriesBurned >= enoughCalories)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {caloriesBurned}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {caloriesBurned}.");
+            }
+
+        }
+    }
+}
