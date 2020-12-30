b99da8204a429f526ea623c1eb899080c086474c
momchilasenov <momchil276@gmail.com>
2020-12-30 19:44:27 +0200

    Added exam problem

---
+++ 08.ExamPreparation/09.PB-Online-Exam-28-and-29-March-2020/01. Supplies for School/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace _01._Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //static input 
+            double pens = 5.80;
+            double markers = 7.20;
+            double liquid = 1.20;
+
+            //Input
+            int numberOfPens = int.Parse(Console.ReadLine());
+            int numberOfMarkers = int.Parse(Console.ReadLine());
+            double cleaningLiquid = double.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+
+            double totalPriceForOrder = numberOfMarkers * markers + numberOfPens * pens + cleaningLiquid * liquid;
+            double discountSum = totalPriceForOrder * discount/100;
+            double needMoney = totalPriceForOrder - discountSum;
+
+            Console.WriteLine($"{needMoney:f3}");
+
+        }
+    }
+}
