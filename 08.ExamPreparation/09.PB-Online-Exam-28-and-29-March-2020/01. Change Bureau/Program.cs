133e1c56797d4106421e1e8246b6840ae36e29c3
momchilasenov <momchil276@gmail.com>
2020-12-30 19:46:25 +0200

    Added exam problem

---
+++ 08.ExamPreparation/09.PB-Online-Exam-28-and-29-March-2020/01. Change Bureau/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBitcoins = int.Parse(Console.ReadLine());
+            double numberOfYuans = double.Parse(Console.ReadLine());
+            double commissionPercentage = double.Parse(Console.ReadLine());
+
+            double dollar = 1.76;
+            double bitcoin = 1168;
+            double yuan = 0.15;
+            double euro = 1.95;
+
+            double bitcoinValue = numberOfBitcoins * bitcoin;
+            double yuanValue = numberOfYuans * yuan*dollar;
+            double totalValue = bitcoinValue + yuanValue;
+            double inEuro = totalValue / euro;
+            double commission = (commissionPercentage / 100) * inEuro;
+            double result = inEuro - commission;
+
+            Console.WriteLine($"{result:f2}");
+
+        }
+    }
+}
