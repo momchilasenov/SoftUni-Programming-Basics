cf93fe266386c797084b421e977cf9ef2ea7c729
momchilasenov <momchil276@gmail.com>
2021-01-02 19:25:31 +0200

    Added exam problem

---
+++ 08.ExamPreparation/09.PB-Online-Exam-28-and-29-March-2020/05. Care of Puppy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Care_of_Puppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFoodInKg = int.Parse(Console.ReadLine());
+            int dogFoodInGrams = dogFoodInKg * 1000;
+
+            string command = Console.ReadLine();
+            int totalFoodEaten = 0;
+
+            while (command != "Adopted")
+            {
+                int foodEatenByDog = int.Parse(command);
+                totalFoodEaten += foodEatenByDog;
+
+                command = Console.ReadLine();
+            }
+            if (totalFoodEaten <= dogFoodInGrams)
+            {
+                int difference = dogFoodInGrams - totalFoodEaten;
+                Console.WriteLine($"Food is enough! Leftovers: {difference} grams.");
+            }
+            else
+            {
+                int foodNeeded = totalFoodEaten - dogFoodInGrams;
+                Console.WriteLine($"Food is not enough. You need {foodNeeded} grams more.");
+            }
+
+        }
+    }
+}
